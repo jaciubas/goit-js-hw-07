0727bbe6a1a9e95587ddbd895e01b5034a23f673
jaciubas <114225522+jaciubas@users.noreply.github.com>
2023-03-01 15:47:31 +0100

    Add files via upload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
+
 galleryItems.forEach((item) => {
   const galleryImg = `<div class="gallery__item">
 <a class="gallery__link" href="${item.original}">
@@ -14,4 +15,21 @@ galleryItems.forEach((item) => {
 </div>`;
 
   gallery.insertAdjacentHTML("beforeend", galleryImg);
+});
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+
+  const instance = basicLightbox.create(
+    `
+        <img width="1400" height="900" src="${event.target.dataset.source}">
+        `
+  );
+  instance.show();
+
+  gallery.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
 });
