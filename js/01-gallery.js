580927aa3419f899f16cc4580f1ba64d84b34da5
jaciubas <114225522+jaciubas@users.noreply.github.com>
2023-03-01 13:29:09 +0100

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,17 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+galleryItems.forEach((item) => {
+  const galleryImg = `<div class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</div>`;
+
+  gallery.insertAdjacentHTML("beforeend", galleryImg);
+});
