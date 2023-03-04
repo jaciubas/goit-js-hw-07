2e48436b58563ad2c1d6f3595be609a4277df1fd
jaciubas <114225522+jaciubas@users.noreply.github.com>
2023-03-04 15:22:55 +0100

    Add files via upload

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,23 +1,17 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 const gallery = document.querySelector(".gallery");
-const galleryItem = galleryItems
-  .map(
-    (item) =>
-      `<div class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
-      <img
-        class="gallery__image"
-        src="${item.preview}"
-        data-source="${item.original}"
-        alt="${item.description}"
-      />
-    </a>
-  </div>`
-  )
-  .join("");
-gallery.innerHTML = galleryItem;
-new SimpleLightbox(".gallery a", {
-  captionDelay: 250,
+
+galleryItems.forEach((item) => {
+  const galleryImg = `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    <div class="gallery__caption">${item.description}</div>
+  </a>`;
+
+  gallery.insertAdjacentHTML("beforeend", galleryImg);
+});
+
+new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionsDelay: 250,
 });
